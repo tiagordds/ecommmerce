31db5d1a0211ff153c8ff8b99471ddb0d9ad8cbe
Tiago Rodrigues <yutri.trds@gmail.com>
2023-12-06 11:07:02 -0300

    models for pedido and itens

--- produto/admin.py
+++ produto/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class VariacaoInLine(admin.TabularInline):
+    model = models.Variacao
+    extra = 1
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines = [
+        VariacaoInLine
+    ]
+
+
+admin.site.register(models.Produto, ProdutoAdmin)
+admin.site.register(models.Variacao)
